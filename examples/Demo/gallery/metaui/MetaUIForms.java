1185cca8857ef8bcd853f8117f01d1af99b75f54
CodexSandboxOffline <zuoneone@gmail.com>
2026-08-30 20:28:48 -0700

    Add WebMCP gallery form example

---
+++ examples/Demo/gallery/metaui/MetaUIForms.java
@@ -0,0 +1,21 @@
+package gallery.metaui;
+
+import ariba.ui.aribaweb.core.AWComponent;
+
+public final class MetaUIForms extends AWComponent
+{
+    public String summary = "";
+    public String details = "";
+    public String ticketMessage;
+
+    public boolean isStateless ()
+    {
+        return false;
+    }
+
+    public AWComponent createTicket ()
+    {
+        ticketMessage = "Ticket ready: " + summary;
+        return null;
+    }
+}
